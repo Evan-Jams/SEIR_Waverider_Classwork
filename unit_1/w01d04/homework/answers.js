5e29b0c025212c5b5468b88bec394c36a7b02a93
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-23 20:49:13 -0500

    Fizz Buzz Answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -23,6 +23,37 @@ for (let i = 0; i <= 200; i++) {
 //---------------//
 //Fizz Buzz
 //---------------//
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log('Fizz');
+  } else {
+    console.log(i);
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0) {
+    console.log('Buzz');
+  } else {
+    console.log(i);
+  }
+}
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz');
+  } else {
+    console.log(i);
+  }
+}
+
+//---------------//
+//Wild Wild Life
+//---------------//
 const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
 const sharky = ["Sharky", "shark", 20, "Left Coast"]
 const plantee = ["Plantee", "plant",  5000 , "Mordor"]
