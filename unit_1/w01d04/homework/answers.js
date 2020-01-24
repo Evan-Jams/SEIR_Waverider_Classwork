3b3d83c5896b38bac16856cee46a7e56a1f75565
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-23 20:41:15 -0500

    Wild Wild Life Answered

--- unit_1/w01d04/homework/answers.js
+++ unit_1/w01d04/homework/answers.js
@@ -19,3 +19,24 @@ for (let i = 0; i <= 200; i++) {
     console.log(i);
   }
 }
+
+//---------------//
+//Fizz Buzz
+//---------------//
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2] = 5001
+console.log(plantee);
+
+wolfy[3] = "Gotham City"
+console.log(wolfy);
+
+dart.push("Hawkins")
+console.log(dart);
+
+wolfy.splice(0, 1, "Gameboy")
+console.log(wolfy);
