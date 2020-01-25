9df75657860151a8a1cdffcc419bf69b2aec227c
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-25 13:39:06 -0500

    Verbal Questions Answered

---
+++ unit_1/w01d05/homework/solutions.js
@@ -0,0 +1,7 @@
+//-----------------------------------------------//
+// Verbal Questions //
+//-----------------------------------------------//
+// 1.) A parameter is the value TO BE evaluated, or the value TO BE inputed (x, num, etc.). An argument IS the inputed value ("string", 50, etc.).
+
+// 2.) A return is the result of the argument that was inputted into the function, and console.log verbalizes that value.
+// ex. [ f(x) = x * 10 ---> f(10) = 10 * 10 returns 100] 'x' is the parameter (number '10' being the argument), 100 is what the return value would be, and console.log would post the retun value in your console.
