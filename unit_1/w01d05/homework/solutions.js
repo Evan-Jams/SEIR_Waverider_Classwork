7c3b472f3b3f1273da85d17f880e62da982c2091
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-25 20:20:04 -0500

    Palindrome Completed

--- unit_1/w01d05/homework/solutions.js
+++ unit_1/w01d05/homework/solutions.js
@@ -5,3 +5,27 @@
 
 // 2.) A return is the result of the argument that was inputted into the function, and console.log verbalizes that value.
 // ex. [ f(x) = x * 10 ---> f(10) = 10 * 10 returns 100] 'x' is the parameter (number '10' being the argument), 100 is what the return value would be, and console.log would post the retun value in your console.
+
+//-----------------------------------------------//
+// Palindrome //
+//-----------------------------------------------//
+// const checkPalindrome = (str) => {
+//   let strReversed = "";
+//   for(let char of str) {
+//     strReversed = char + strReversed;
+//   } if (strReversed == str) {
+//     return "Is A Palindrome"
+//   } else {
+//     return "Is NOT A Palindrome"
+//   }
+// }
+//--------First Attempt-------------//
+
+const checkPalindrome = (str) => {
+  let lowerCaseStr = str.toLowerCase();
+  let reverseStr = lowerCaseStr.split("").reverse().join("");
+  return lowerCaseStr === reverseStr
+}
+console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("Borscht"));
+//---------Simpler Solution---------//
