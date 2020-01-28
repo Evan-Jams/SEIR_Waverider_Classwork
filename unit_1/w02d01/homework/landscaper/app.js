9ee0e6b22eaa45770521c99f85caf61af4c31556
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-27 19:58:41 -0500

    trying a different approach to the game, can use teeth to cut grass though still

--- unit_1/w02d01/homework/landscaper/app.js
+++ unit_1/w02d01/homework/landscaper/app.js
@@ -1,43 +1,96 @@
-console.log('connected');
+// console.log('connected');
+// alert('You are starting a landscaping business, but all you have are your teeth')
+
+
+//make arrays/objects of tools w/ cost, pay grade, etc.
+const tools = [
+
+{
+  name: 'Teeth',
+  cost: 0,
+  payout: 1
+},
+
+{
+  name: 'Rusty Scissors',
+  cost: 5,
+  payout: 5
+},
+
+{
+  name: 'Old-timey Push Lawnmower',
+  cost: 25,
+  payout: 50
+},
+
+{
+  name: 'New Lawn Mower',
+  cost: 250,
+  payout: 100
+},
+
+{
+  name: 'Team of Starving Students',
+  cost: 500,
+  payout: 250
+}
+]
+
+
+// console.log(tools[0 + 1].name)
+//Write a function that adds one to the index of tools array
+const upgraded = (i) => {
+  return (tools[i + 1].name)
+}
+
+// console.log(upgraded(0));
+
+
+
+
 alert('You are starting a landscaping business, but all you have are your teeth')
 
-let teeth
+
 let money
 
 const start = () => {
-  teeth = 'your'
+  tool = tools[0].name
   money = 0
 
   askForAction()
 }
 
 const showStatus = () => {
-  alert(`you have ${teeth} teeth and $${money}`)
+  alert(`You are using your ${tool} and have $${money}`)
 }
 
 const askForAction = () => {
   showStatus()
-  let choice = prompt('What do you want to do?', '(u)se teeth / (b)uy / (r)estart')
+  let choice = prompt('What do you want to do?', `(1) Use ${tool}  / (2) Upgrade tool / (3) Restart`)
   console.log(choice);
   //make cancel button work
-  if (!choice || choice === 'r') {
-    alert('ok! no more money')
+  if (!choice) {
+    alert('Uh Oh! You\'ve ended your progress!')
     return
   }
-
-
-if (choice === 'u') {
-    useTeeth();
-  } else if (choice === 'r') {
-    console.log('restarted');
+  //make choices for options
+  if (choice === '1') {
+    cutGrass();
+  } else if (choice === '2'){
+    getUpgrade();
+  } else if (choice === '3') {
+    console.log('Restarted');
   } else {
     console.log('uh oh! somethings wrong');
   }
 }
 
-const useTeeth = () => {
+const cutGrass = () => {
   money++
   askForAction()
 }
 
+const getUpgrade = () => {
+  askForAction()
+}
 start()
