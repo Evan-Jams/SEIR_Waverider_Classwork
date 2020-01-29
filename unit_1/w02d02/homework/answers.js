81249b368f34fd7240e8fccc96dee1779bfda61f
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-28 20:43:02 -0500

    datatype refresher complete

---
+++ unit_1/w02d02/homework/answers.js
@@ -0,0 +1,39 @@
+//===========================================//
+//========= Data type refresher =============//
+1. const light = true
+2. const email = user@something.com
+3. const spaceship = ['hull', 'laser blasters', 'tractor beam', 'warp drive']
+4. const class = {
+     studentNames: ['John', 'Jamal', 'Ben',]
+   }
+5. const classRoom = {
+     studentNames: {
+       John: {
+         location: 'city'
+       },
+       Jamal: {
+         location: 'small town'
+       },
+       Ben: {
+         location: 'woods'
+       },
+     }
+   }
+
+6. const classRoom = {
+     studentNames: [{
+       John: {
+          location: 'city',
+          favoriteShows: ['one', 'two', 'three']
+     },
+       Jamal: {
+          location: 'small town',
+          favoriteShows: ['one', 'two', 'three']
+     },
+       Ben: {
+          location: 'woods',
+          favoriteShows: ['one', 'two', 'three']
+     },
+     }
+   ]
+   }
