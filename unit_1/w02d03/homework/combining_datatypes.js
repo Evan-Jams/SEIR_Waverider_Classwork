65acc2364d1b976c63a91f19a1c678bdeb570cb6
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-29 21:14:38 -0500

    section 2 complete

--- unit_1/w02d03/homework/combining_datatypes.js
+++ unit_1/w02d03/homework/combining_datatypes.js
@@ -57,3 +57,56 @@ const powerButton = () => {
 
 console.log(powerButton());
 //---------------------------------------
+
+
+const add = (num1, num2) => {
+  return (num1 + num2)
+}
+
+const subtract = (num1, num2) => {
+  return (num1 - num2)
+}
+
+const multiply = (num1, num2) => {
+  return (num1 * num2)
+}
+
+const divide = (num1, num2) => {
+  return (num1 / num2)
+}
+
+const calculate = (num1, num2, operates) => {
+  return (operates(num1, num2))
+}
+
+console.log(calculate(1, 2, add));
+console.log(calculate(1, 2, subtract));
+console.log(calculate(1, 2, multiply));
+console.log(calculate(1, 2, divide));
+//-------------------------------------------------------
+
+
+
+// const bar = () => {
+//     console.log('bar here');
+// }
+//
+// const foo = () => {
+//     console.log('foo here');
+// }
+//
+// bar();
+// foo();
+//-------------------------------------------------------
+
+
+const foo = () => {
+    console.log('hi');
+}
+
+foo();
+
+// the error is 'SyntaxError: Missing initializer in const declaration'
+// meaning the const foo hasn't been declared. by putting an '=' after 'foo',
+// this solves that issue, then you have to call the function after the
+// function declaration. so move 'foo();' below the 'const foo'
