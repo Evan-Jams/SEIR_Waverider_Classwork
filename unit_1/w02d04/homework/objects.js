92186e6e41ec6801b2c574a262469e7e1892682e
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-30 19:53:37 -0500

    Created Person class

--- unit_1/w02d04/homework/objects.js
+++ unit_1/w02d04/homework/objects.js
@@ -1,8 +1,8 @@
 class Hamster {
-  constructor(owner, name, price){
+  constructor(owner, name){
     this.owner = owner;
     this.name = name;
-    this.price = price;
+    this.price = 15;
   }
   wheelRun(){
     console.log("squeak squeak");
@@ -14,3 +14,52 @@ class Hamster {
     return this.price
   }
 }
+
+class Person {
+  constructor(name){
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamsters = [];
+    this.bankAccount = 0;
+  }
+  getName(){
+    return this.name
+  }
+  getAge(){
+    return this.age
+  }
+  getWeight(){
+    return this.weight
+  }
+  greet(){
+    console.log(`Hi, my name is ${name}`);
+  }
+  eat(){
+    this.weight++
+    this.mood++
+  }
+  exercise(){
+    this.weight--
+  }
+  ageUp(){
+    this.age++
+    this.height++
+    this.weight++
+    this.mood--
+    this.bankAccount + 10
+  }
+  buyHamster(){
+    const newHamster = new Hamster(this.name, 'ham' + this.hamsters.length)
+    this.hamsters.push(newHamster);
+    this.mood += 10
+    this.bankAccount -= newHamster.getPrice()
+
+  }
+}
+
+const evan = new Person('Evan');
+
+console.log(evan);
