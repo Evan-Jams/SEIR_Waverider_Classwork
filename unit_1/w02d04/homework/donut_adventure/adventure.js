6a0b358e51649dbfaafaad9176bbbbb908d045e1
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-01-30 22:52:53 -0500

    Created Pizza Rat

--- unit_1/w02d04/homework/donut_adventure/adventure.js
+++ unit_1/w02d04/homework/donut_adventure/adventure.js
@@ -24,4 +24,28 @@ class Hero {
 
 const dougie = new Hero('Dougie the Donut');
 
-dougie.talkSass()
+// dougie.talkSass()
+
+
+class Enemy {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    };
+    this.catchPhrases = ['i\'m youtube famous', 'i\'m more dangerous that an uncovered sewer'];
+  }
+  talkSmack() {
+    console.log(this.catchPhrases[Math.floor(Math.random() * this.catchPhrases.length)]);
+  }
+  announceHealth() {
+    console.log(this.health);
+  }
+  fight() {
+    console.log('i\'m gonna flatten you like a slice of pepperoni!');
+  }
+}
+
+const pizzaRat = new Enemy('Pizza Rat');
