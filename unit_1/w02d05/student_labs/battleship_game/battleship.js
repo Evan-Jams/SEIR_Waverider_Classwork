ca6dab119247587aff338273a8e95817998e3fa1
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-02-01 19:18:18 -0500

    battleship_reformed.js is the working one, just a couple small bugs to iron out

--- unit_1/w02d05/student_labs/battleship_game/battleship.js
+++ unit_1/w02d05/student_labs/battleship_game/battleship.js
@@ -1,4 +1,6 @@
 // console.log('hi');
+//========================== SpaceShip Class ================================//
+
 
 class SpaceShip {
     constructor(name, hull, firepower, accuracy) {
@@ -7,29 +9,65 @@ class SpaceShip {
         this.firepower = firepower;
         this.accuracy = accuracy;
 }
-    attack(enemy) {
-    if (Math.random() < enemy.accuracy) {
-        console.log(`${enemy} has been hit!`);
-        enemy.takeDamage()
+    takeDamage(target){
+        target.hull -= this.firepower;
+        if (target.hull <= 0) {
+        console.log(target.name, 'has no more hull remaining.');
+        console.log(target.name, 'is destroyed!');
+    } else if (target.hull > 0) {
+            target.attack(this.name)
+            // this.hull -= enemy.firepower;
+            console.log(this.name, 'is down to', this.hull, 'remaining.');
+        }
+
+    }
+    attack(target) {
+    if (Math.random() < this.accuracy) {
+        console.log(target.name, `has been hit!`);
+        this.takeDamage(target);
     } else {
         console.log(`Oh no! You missed`);
+        target.this.attack(this)
         }
     }
-    takeDamage(){
-        this.hull -= enemy.firepower;
-        console.log(this.name, 'is down to', this.hull, 'remaining.');
-    }
+    // takeDamage(enemy){
+    //     enemy.hull -= this.firepower;
+    //     console.log(enemy.name, 'is down to', enemy.hull, 'remaining.');
+    //     if (enemy.hull > 0) {
+    //         enemy.this.attack(this.name)
+    //     }
+    // }
+    // takeDamage(target) {
+    //         target.hull -= this.firepower;
+    //         if (target.hull <= 0) {
+    //         console.log(target.name, 'has no more hull remaining.');
+    //         console.log(target.name, 'is destroyed! Nice work!');
+    //     } else if (target.hull > 0) {
+    //             this.attack(this.name)
+    //             this.hull -= target.firepower;
+    //             console.log(this.name, 'is down to', this.hull, 'remaining.');
+    //         }
+    //
+    //     }
     retreat() {
         console.log('I didn\'t know you were afraid to die for your planet!');
     }
 }
-// Make USS Schwarzenegger
+
+
+
+
+//========================== Make USS Schwarzenegger ==========================//
+
 const ussSchwarzenegger = new SpaceShip('USS Schwarzenegger', 20, 5, .7)
-console.log(ussSchwarzenegger);
+// console.log(ussSchwarzenegger);
 
 // ussSchwarzenegger.attack()
 //
-// Make Alien ships
+
+
+//=========================== Make Alien ships ================================//
+
 class Enemy extends SpaceShip {
     constructor(name, hull, firepower, accuracy) {
         super(name, hull, firepower, accuracy)
@@ -40,11 +78,11 @@ class Enemy extends SpaceShip {
     }
 }
 
-
+//=========================== Alien ship factory ==============================//
 class AlienFactory {
     constructor(ship) {
         this.ship = ship;
-        this.aliens = [];
+        this.fleet = [];
         this.hull = [3, 4, 5, 6];
         this.firepower = [2, 3, 4];
         this.accuracy = [.6, .7, .8];
@@ -53,36 +91,75 @@ class AlienFactory {
         return Math.floor(Math.random() * array.length);
     }
     generateEnemy() {
-        const newAlienShip = new Enemy(this.ship + this.aliens.length, this.hull[this.randomInt(this.hull)], this.firepower[this.randomInt(this.firepower)], this.accuracy[this.randomInt(this.accuracy)])
-        this.aliens.push(newAlienShip);
-        console.log(newAlienShip);
+        const newAlienShip = new Enemy(this.ship + this.fleet.length, this.hull[this.randomInt(this.hull)], this.firepower[this.randomInt(this.firepower)], this.accuracy[this.randomInt(this.accuracy)])
+        this.fleet.push(newAlienShip);
+        // console.log(newAlienShip);
     }
 }
+
+
+//============================== Action functions (outer functions attempt) =============================//
+
+// const attack = (enemy) => {
+//     if (Math.random() < ussSchwarzenegger.accuracy) {
+//         console.log(enemy.name, `has been hit!`);
+//         takeDamage(enemy);
+//     } else {
+//         console.log(`Oh no! You missed`);
+//         attack(ussSchwarzenegger)
+//         takeDamage(ussSchwarzenegger)
+//     }
+//     }
+//
+// const takeDamage = (enemy) => {
+//         enemy.hull -= ussSchwarzenegger.firepower;
+//         if (enemy.hull <= 0) {
+//         console.log(enemy.name, 'has no more hull remaining.');
+//         console.log(enemy.name, 'is destroyed! Nice work!');
+//         } else if (enemy.hull > 0) {
+//             attack(ussSchwarzenegger)
+//             this.hull -= enemy.firepower;
+//             console.log(this.name, 'is down to', this.hull, 'remaining.');
+//         }
+//
+//     }
+
+
+
+
+
+
 
 
 const alien = new AlienFactory('Alien')
 for (let i = 0; i < 6; i++) {
 alien.generateEnemy();
 }
+// console.log(AlienFactory.aliens);
+// console.log(alien.aliens)
 
-Make a ship class
+// ussSchwarzenegger.attack(alien.aliens[0])
+
+//Make intro and choices
 
 const welcomeStatement = () => {
-     alert('Welcome to Space Battle warrior! Guide us to Victory!')
+     alert('Welcome to Space Battle, warrior! Guide us to Victory!')
 }
 
 const showStatus = () => {
-     alert(`Your ship is the ${ussSchwarzenegger.name}`)
+     alert(`Your ship is the ${ussSchwarzenegger.name}, your hull is ${ussSchwarzenegger.hull}. Don\'t let us down!`)
+
 }
 
 const askForAction = () => {
-     let choice = prompt('(1) Attack Enemy ship / (2) Retreat')
+     let choice = prompt('(1) Engage in battle')
+     // let choice2 = prompt('(1) Attack Enemy ship / (2) Retreat like a wimp')
      console.log(choice);
      if (!choice) {
-         alert('Sorry, that is not a valid option, try again')
-         askForAction()
+         alert('Sorry, that is not a valid option')
+         // askForAction()
      } else if (choice === '1') {
-         ussSchwarzenegger.attack();
+         ussSchwarzenegger.attack(alien.fleet[0]);
      } else if (choice === '2') {
          ussSchwarzenegger.retreat();
      }
@@ -95,4 +172,7 @@ const start = () => {
 }
 
 // console.log(alien.randomInt(alien.firepower));
-// start()
+start()
+
+
+//
---
+++ unit_1/w02d05/student_labs/battleship_game/battleship_reformed.js
@@ -0,0 +1,159 @@
+// Make a Spaceship class for framework of my USS Schwarzenegger and Enemy factory
+class SpaceShip {
+    constructor(name, hull, firepower, accuracy) {
+        this.name = name;
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+}
+    attack(enemy){
+        // if random value is less than accuracy, then attack first alien, alien.fleet[0]
+        if (Math.random() < this.accuracy) {
+
+            console.log(enemy.name, 'has been hit!');
+            // enemy.hull -= this.firepower;
+            enemy.takeDamage();
+
+            // else, log that you missed
+            // then call the enemy to retaliate
+        } else {
+            console.log('Oh no! You missed!');
+            enemy.attack();
+        }
+
+    }
+    takeDamage(enemy) {
+        // If alien ship survives or if missed attack, subtract alien's firepower from hull
+        this.hull -= enemy.firepower
+        if (this.hull > 0) {
+            console.log('You have been hit by', enemy.name, '! It has done', enemy.firepower, 'damage. You have', this.hull, 'hull left.');
+            this.attack(enemy)
+        }
+        // If hull remains > 0, attack again.
+        // If hull reaches 0, alert loss of game.
+    }
+}
+
+// Making an Enemy Factory
+class Enemy {
+    constructor(name, hull, firepower, accuracy) {
+        this.name = name;
+        this.hull = hull;
+        this.firepower = firepower;
+        this.accuracy = accuracy;
+    }
+    attack() {
+        // if random number is less than this accuracy, hit ussSchwarzenegger
+        if (Math.random() < this.accuracy) {
+            ussSchwarzenegger.takeDamage(this)
+
+        } else {
+            console.log('Alien ship has missed his attack');
+            ussSchwarzenegger.attack(this)
+        }
+        // else log that alien missed
+    }
+    takeDamage() {
+        // subtract ussSchwarzenegger's firepower from hull
+        this.hull -= ussSchwarzenegger.firepower
+        // if hull is <= 0 alert destroyed, good job
+        if (this.hull <= 0) {
+            console.log('Alien ship destroyed. Nice work!');
+        } else {
+            console.log('Alien has', this.hull, 'hull left');
+            this.attack(ussSchwarzenegger);
+        }
+        // If hull is > 0 after first attack from ussSchwarzenegger, attack
+    }
+    retreat() {
+        alert ('Afraid to die for your planet huh?');
+    }
+}
+
+
+class AlienFactory {
+    constructor(ship) {
+        this.ship = ship;
+        this.fleet = [];
+        this.hull = [3, 4, 5, 6];
+        this.firepower = [2, 3, 4];
+        this.accuracy = [.6, .7, .8];
+    }
+    randomInt(array) {
+        return Math.floor(Math.random() * array.length);
+    }
+    generateEnemy() {
+        const newAlienShip = new Enemy(this.ship + ' ' + this.fleet.length, this.hull[this.randomInt(this.hull)], this.firepower[this.randomInt(this.firepower)], this.accuracy[this.randomInt(this.accuracy)])
+        this.fleet.push(newAlienShip);
+    }
+}
+
+
+//==================== USS Schwarzenegger & Alien Fleet =========================//
+
+const ussSchwarzenegger = new SpaceShip('USS Schwarzenegger', 20, 5, .7)
+// console.log(ussSchwarzenegger);
+
+
+const alien = new AlienFactory('Alien')
+for (let i = 0; i < 6; i++) {
+alien.generateEnemy();
+}
+
+// ussSchwarzenegger.attack(alien.fleet[0]);
+
+//======================================================================================//
+//===================== Alerts, prompts, and choices ===================================//
+//======================================================================================//
+
+const welcomeStatement = () => {
+    alert('Welcome to Space Battle! Guide us to Victory!')
+}
+
+const showStatus = () => {
+    alert(`Your ship is the ${ussSchwarzenegger.name}, your hull is ${ussSchwarzenegger.hull}. Don\'t let us down!`)
+
+}
+
+let i = 1;
+
+const askToContinue = () => {
+    let choice2 = prompt('(1) Attack Enemy ship / (2) Retreat like a wimp');
+    if (choice2 === null) {
+        alert('Sorry to see you go, thank you for your service!')
+        return('Player has left')
+    } else if (choice2 === '1') {
+        do {
+            ussSchwarzenegger.attack(alien.fleet[i]);
+            i += 1;
+            askToContinue();
+        } while (i < (alien.fleet.length - 1));
+            alert('Congradulations Warrior! You have saved the planet!!');
+            return('Player has won!');
+    } else if (choice2 === '2') {
+        ussSchwarzenegger.retreat();
+    }
+}
+
+const askForAction = () => {
+    let choice = prompt('(1) Engage in battle')
+
+    // let choice2 = prompt('(1) Attack Enemy ship / (2) Retreat like a wimp')
+    console.log(choice);
+    if (choice === null) {
+        alert('Sorry to see you go');
+    } else if (!choice) {
+        alert('Sorry, that is not an option. Try that again');
+        askForAction();
+    } else if (choice === '1') {
+        ussSchwarzenegger.attack(alien.fleet[0]);
+        askToContinue();
+    }
+}
+const start = () => {
+     welcomeStatement()
+     showStatus()
+     askForAction()
+}
+
+start()
