3634af80472e704873a9ed20bb21eadfa4307180
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-02-03 18:39:44 -0500

    added more elements. My power grows with my dedication and hard work

--- unit_1/w03d01/homework/hogwarts/app.js
+++ unit_1/w03d01/homework/hogwarts/app.js
@@ -1,6 +1,8 @@
 // if (typeof jQuery == 'undefined'){
 //   console.log('oops! I still have to link my jQuery properly!');
 // } else {console.log('I did it! I linked jQuery and this js file!')};
+//Holly Wand with Unicorn Hair Core
+
 $(() => {
     const $body = $('body');
     const $container = $('#container');
@@ -8,7 +10,17 @@ $(() => {
 
     const $h1 = $('<h1>')
         .text('Hogwarts');
+    const $h2 = $('<h2>')
+        .text('Evan');
+    const $h3 = $('<h3>')
+        .text('Gryffindor');
+    const $h4 = $('<h4>')
+        .addClass('cat')
+        .text('Dill');
 
-    $body.append($h1);
-    
+
+
+
+    $body.append($h1, $h2, $h3, $h4);
+
 })
