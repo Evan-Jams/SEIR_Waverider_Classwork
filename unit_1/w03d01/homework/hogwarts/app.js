47d9dc3e98f31505ab74122703f3db45ad419db7
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-02-03 22:11:23 -0500

    I added list items. Code is not very dry, tried to write a for loop, but could not get the classes incorporated

--- unit_1/w03d01/homework/hogwarts/app.js
+++ unit_1/w03d01/homework/hogwarts/app.js
@@ -17,10 +17,55 @@ $(() => {
     const $h4 = $('<h4>')
         .addClass('cat')
         .text('Dill');
+    const $h4_2 = $('<h4>')
+        .text('Holly Wand with Unicorn Hair Core')
+
+    const $ul = $('<ul>').attr('storage', 'trunk');
 
 
+    const $li_1 = $('<li>').text('Butter Beer');
+    const $li_2 = $('<li>').text('Invisibility Cloak').addClass('secret');
+    const $li_3 = $('<li>').text('Magic Map').addClass('secret');
+    const $li_4 = $('<li>').text('Time Turner').addClass('secret');
+    const $li_5 = $('<li>').text('Leash').addClass('cat');
+    const $li_6 = $('<li>').text('Magic Map');
+
+    $ul.append($li_1, $li_2, $li_3, $li_4, $li_5, $li_6);
+
+//                         Skip This, Failed attempt                    //
+//========================================================================//
+// Used a for loop to add all the list items at once, but then couldn't access
+// individual list items to add a class to them, don't know why that was, but
+// I gave up on it
+//======================================================================//
+
+    // const $storageTrunk = $([
+    //     'butter beer',
+    //     'invisibility cloak',
+    //     'magic map',
+    //     'time turner',
+    //     'leash',
+    //     'Bertie Bott\'s Every Flavor [Jelly] Beans'
+    // ]);
+
+        // for (let i = 0; i < 6; i++) {
+        //     if (i !== 0 && i <= 3) {
+        //         $ul.eq(i).append($('<li>').text($storageTrunk[i])).addClass('secret')
+        //     } else if (i = 4) {
+        //         $ul.eq(i).append($('<li>').text($storageTrunk[i])).addClass('cat')
+        //     } else {
+        //         $ul.eq(i).append($('<li>').text($storageTrunk[i]))
+        //     }
+        // };
+    // console.log($li);
+    // console.log($storageTrunk);
+    // console.log($ul);
+    // console.log($body);
+//===========================================================================//
+//===========================================================================//
 
 
-    $body.append($h1, $h2, $h3, $h4);
+    $container.append($h1, $h2, $h3, $h4, $h4_2, $ul)
+    // $body.append();
 
 })
