130250544ca57725cf9d16e437b09f51781c7317
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-02-05 13:32:22 -0500

    got the numbers to log in console and be put into array

--- unit_1/w03d02/homework/calculator/app.js
+++ unit_1/w03d02/homework/calculator/app.js
@@ -1,1 +1,14 @@
-console.log('connected');
+// console.log('connected');
+
+let numberDisplay = []
+
+
+$(() => {
+    const $buttons = $('.button');
+    // Logging the value of the button
+    $buttons.on('click', (event) => {
+        console.log($(event.currentTarget).attr('value'));
+        numberDisplay.push($(event.currentTarget).attr('value'));
+        console.log(numberDisplay);
+    })
+})
