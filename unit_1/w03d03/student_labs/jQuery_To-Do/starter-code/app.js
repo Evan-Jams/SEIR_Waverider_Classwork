5d945caf2aab987fa3a734344ecb8104414886dc
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-02-06 09:27:06 -0500

    Couldnt get remove to work

--- unit_1/w03d03/student_labs/jQuery_To-Do/starter-code/app.js
+++ unit_1/w03d03/student_labs/jQuery_To-Do/starter-code/app.js
@@ -22,11 +22,16 @@ const render = () => {
     $newToDo.empty()
     list.forEach((toDo) => {
         const $li = $('<li>').text(toDo.name).addClass('to-do-item');
+        let $btn = $('<button>').text('Completed').appendTo($li)
         $newToDo.append($li);
         $li.on('click', (event) => {
             $(event.currentTarget).appendTo($newToDone).removeClass().addClass('done-item')
+            $btn.text('Remove').appendTo($li)
             const index = $(event.currentTarget).index()
             list[index].declined = true
         })
+        // if (toDo.declined) {
+        //     $newToDone.remove()
+        // }
     })
 }
