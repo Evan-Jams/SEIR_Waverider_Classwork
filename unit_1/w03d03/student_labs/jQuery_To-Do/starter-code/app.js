d7ab56812dc395b8bcafdbfd276015d1013221e4
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-02-05 19:49:54 -0500

    Got the list item to populate the row and go to completed column when clicked

--- unit_1/w03d03/student_labs/jQuery_To-Do/starter-code/app.js
+++ unit_1/w03d03/student_labs/jQuery_To-Do/starter-code/app.js
@@ -1,7 +1,6 @@
 const list = []
 
 
-
 $(() => {
     $('form').on('submit', (event) => {
         event.preventDefault()
@@ -10,9 +9,19 @@ $(() => {
         $(event.currentTarget).trigger('reset')
         console.log(inputValue);
         //attach item to the page
+        render();
     })
 })
 
 const render = () => {
-    
+    $('ul').empty()
+    list.forEach((toDo) => {
+        const $li = $('<li>').text(toDo.name).addClass('to-do-item');
+        $('#to-do-list').append($li);
+        $li.on('click', (event) => {
+            $(event.currentTarget).appendTo($('#completed')).removeClass().addClass('done-item')
+            const index = $(event.currentTarget).index()
+            list[index].declined = true
+        })
+    })
 }
