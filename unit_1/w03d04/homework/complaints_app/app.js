d3a75be9962b989e9837a4f6ff4201bebaabe621
Evan James <evanjames@Alexiss-MacBook-Pro-2.local>
2020-02-07 08:28:48 -0500

    couldnt figure out how to use the info into working code to manipulate

--- unit_1/w03d04/homework/complaints_app/app.js
+++ unit_1/w03d04/homework/complaints_app/app.js
@@ -1,38 +1,41 @@
 // console.log($);
 // console.log('hi');
 
-// https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough=BRONX
-// https://data.cityofnewyork.us/resource/erm2-nwe9.json
 
-// $.ajax({
-//     url: 'https://data.cityofnewyork.us/resource/erm2-nwe9.json?agency=NYPD',
-//     success: (data) => {
-//         console.log(data);
-//     },
-//     error: () => {
-//         console.log('you done messed up');
-//     }
-// })
-
-const randomInt = (limit) => {
-    return Math.floor(Math.random() * limit)
+const randomInt = () => {
+    return Math.floor(Math.random() * data.length)
 }
 
 $(() => {
     $.ajax({
         url: 'https://data.cityofnewyork.us/resource/erm2-nwe9.json'
     }).then((data) => {
-        const $index = () => {
-
+        // I know this isn't even close to being correct. Trying to produce a number of complaints based on the number placed into the input, randomly drawn from the giant database.
+        for (let i = 0; i < $('#input-box.input[input.val()]'); i++) {
+            let i = randomInt()
+            console.log(data[i].complaint_type);
         }
-        console.log(data[1].borough);
-        console.log(data[1].agency);
-        console.log(data[1].complaint_type);
-        console.log(data[1].descriptor)
-        console.log(data[99]);
+
+        // Get the borough info
+        $('#'+ data[i].borough).html();
+        // Get agency info
+        // $('.incidents').html(data[i].agency);
+        // Get complaint type
+        // $('.incidents').html(data[i].complaint_type);
+        // get decription of the complaint
+        // $('.incidents').html(data[i].descriptor)
+        // console.log(data[99]);
 
     },
     () => {
         console.log('this isnt working out');
     }
 )});
+
+
+//===========================================================================//
+// Maybe just exhaustion or something, but I just could not put the pieces together
+// tonight. Basically just stared at my screen for 5 hours last night and a few this morning
+// trying to figure out how to go about getting the information and using it towards a page.
+// Hence why my styling and functionality are severely lacking.
+//===========================================================================//
