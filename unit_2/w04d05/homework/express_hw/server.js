b978b9bb74bd593243172de31623cf6af9b503f1
Evan James <thevan777@gmail.com>
2020-02-15 18:13:30 -0500

    Magic 8 ball express app created

--- unit_2/w04d05/homework/express_hw/server.js
+++ unit_2/w04d05/homework/express_hw/server.js
@@ -18,6 +18,18 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send("Based on your $" + req.params.total + " bill, a " + req.params.tipPercentage + "% tip will be $" + sum)
 })
 
+// Magic 8 ball *********************************//
+//********************************************//
+const magicResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+const randomInt = () => {
+    return Math.floor(Math.random() * (magicResponses.length));
+}
+
+app.get('/magic/:question', (req, res) => {
+    let newHtml = `<h1>${magicResponses[randomInt()]}</h1>`
+    res.send(req.params.question + "?: " + newHtml)
+})
 
 
 
