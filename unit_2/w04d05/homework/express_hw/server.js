efb5afdc84e2a33c2512bc32cad17691a736dc96
Evan James <thevan777@gmail.com>
2020-02-15 13:09:12 -0500

    Greeting express application created

---
+++ unit_2/w04d05/homework/express_hw/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+
+app.get('/greeting/:name', (req, res) => {
+    res.send("Hello, " + req.params.name + "! It's so great to see you!")
+})
+
+app.listen(PORT, () => {
+    console.log('Someone is listening...');
+})
