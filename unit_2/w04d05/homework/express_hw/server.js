d98b65c628de4096bd9119df1ae6db1119054f13
Evan James <thevan777@gmail.com>
2020-02-15 13:55:04 -0500

    Tip Calculator express application created

--- unit_2/w04d05/homework/express_hw/server.js
+++ unit_2/w04d05/homework/express_hw/server.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-
+// Greetings *********************************//
+//********************************************//
 app.get('/greeting/:name', (req, res) => {
     res.send("Hello, " + req.params.name + "! It's so great to see you!")
 })
+
+// Tip Calculator *********************************//
+//********************************************//
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let num1 = parseInt(req.params.total)
+    let num2 = (parseInt(req.params.tipPercentage) / 100)
+    let sum = (num1 * num2)
+
+    res.send("Based on your $" + req.params.total + " bill, a " + req.params.tipPercentage + "% tip will be $" + sum)
+})
+
+
+
 
 app.listen(PORT, () => {
     console.log('Someone is listening...');
