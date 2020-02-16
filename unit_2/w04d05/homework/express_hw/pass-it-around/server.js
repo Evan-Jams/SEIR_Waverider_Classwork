fbb0fb79980f767b83b6a3bd89ebad01cbc55e07
Evan James <thevan777@gmail.com>
2020-02-16 13:32:52 -0500

    Completed Bottles on the wall

--- unit_2/w04d05/homework/express_hw/pass-it-around/server.js
+++ unit_2/w04d05/homework/express_hw/pass-it-around/server.js
@@ -2,16 +2,25 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+
 let bottles = 99
-const html = (`<a href="/${bottles}">take one down, pass it around</a>`)
+let html = (`<a href="/${bottles - 1}">take one down, pass it around</a>`)
+
 
 app.get('/', (req, res) => {
-    res.send(`99 Bottles of beer on the wall` + `: ` + html)
+    res.send(`${bottles} Bottles of beer on the wall` + `: ` + html)
 })
 
 app.get(`/:number_of_bottles`, (req, res) => {
-    // bottles -= 1
-    res.send(`${bottles} Bottles of beer on the wall` + `: ` + html)
+    let bottles = req.params.number_of_bottles
+    let html = (`<a href="/${bottles - 1}">take one down, pass it around</a>`)
+    if (bottles > 0) {
+        res.send(`${bottles} Bottles of beer on the wall` + `: ` + html)
+    } else {
+        let html = `<a href="/">Go home to refill</a>`
+        res.send(`${bottles} Bottles of beer on the wall` + `: ` + html)
+    }
+
 })
 
 
