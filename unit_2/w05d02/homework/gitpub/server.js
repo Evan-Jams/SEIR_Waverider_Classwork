36d0f309b05c96a49973dd0b845546379e389348
Evan James <thevan777@gmail.com>
2020-02-18 19:40:04 -0500

    setup basic structure for gitpub app

---
+++ unit_2/w05d02/homework/gitpub/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, (req, res) => {
+    console.log('Careful what you type.. Someone is listening...');
+})
