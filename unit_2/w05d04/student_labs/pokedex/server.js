f4fa6f135e462d79358ba3c3701d6320213699bd
Evan James <thevan777@gmail.com>
2020-02-20 17:50:30 -0500

    made new, edit, delete, update, and create routes. Yet to make associated pages

--- unit_2/w05d04/student_labs/pokedex/server.js
+++ unit_2/w05d04/student_labs/pokedex/server.js
@@ -9,19 +9,50 @@ app.use(methodOverride('_method'))
 
 const pokemon = require('./models/pokemon.js')
 
+// index
 app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {
         pokemon: pokemon
     })
 })
 
+// New
+app.get('/pokemon/new', (req, res) => {
+  res.render('new.ejs');
+})
+
+// Show
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
         pokeman: pokemon[req.params.id]
     })
 })
 
+// Edit
+app .get('/pokemon/:index/edit', (req, res) => {
+    res.render('edit.ejs', {
+        pokeman: pokemon[req.params.index],
+        index: req.params.index
+    })
+})
 
+//Delete
+app.delete('/pokemon/:index', (req, res) => {
+    pokemon.splice(req.params.index, 1)
+    res.redirect('/pokemon')
+})
+
+// Update
+app.put('/pokemon/:index', (req, res) => {
+    pokemon[req.params.index] = req.body
+    res.redirect('/pokemon')
+})
+
+// Create
+app.post('/pokemon', (req, res) => {
+  pokemon.push(req.body)
+  res.redirect('/pokemon')
+})
 
 
 
