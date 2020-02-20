6e43b21f21027d845212408ac39db5505a478ba1
Evan James <thevan777@gmail.com>
2020-02-20 13:32:39 -0500

    image of pokemon on index page, plus image on show page with info about pokemon

--- unit_2/w05d04/student_labs/pokedex/server.js
+++ unit_2/w05d04/student_labs/pokedex/server.js
@@ -1,6 +1,28 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express()
 const PORT = 3000
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
+
+const pokemon = require('./models/pokemon.js')
+
+app.get('/pokemon', (req, res) => {
+    res.render('index.ejs', {
+        pokemon: pokemon
+    })
+})
+
+app.get('/pokemon/:id', (req, res) => {
+    res.render('show.ejs', {
+        pokeman: pokemon[req.params.id]
+    })
+})
+
+
+
 
 
 
