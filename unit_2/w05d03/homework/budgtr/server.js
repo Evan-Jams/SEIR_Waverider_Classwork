de60aa9c76d12d0708fc1012352a78c04e405ee1
Evan James <thevan777@gmail.com>
2020-02-19 21:47:10 -0500

    Got basic index get function set up and downloaded sketeton

--- unit_2/w05d03/homework/budgtr/server.js
+++ unit_2/w05d03/homework/budgtr/server.js
@@ -4,6 +4,25 @@ const PORT = 3000
 
 const budget = require('./models/budget.js')
 
+// Middleware
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    // res.send('Hey, whats up')
+    res.render('index.ejs', {
+        
+    })
+})
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log('Careful...Someone is listening...');
