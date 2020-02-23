f607561873dfba8c1322a594487de68d0631f47c
Evan James <thevan777@gmail.com>
2020-02-23 14:51:45 -0500

    Finished homework

--- unit_2/w05d05/homework/hotels_app/app.js
+++ unit_2/w05d05/homework/hotels_app/app.js
@@ -15,7 +15,7 @@ mongoose.connect(mongoURI, {
     useFindAndModify: false
     },
     () => {
-        console.log('Yo, you are connected homie');
+        console.log('You are connected homie');
 })
 
 // error & success
@@ -44,3 +44,145 @@ setTimeout(() => {
 //   if (err) console.log(err.message)
 //   console.log(`There are ${data} hotels in this database`)
 // })
+
+const myFirstHotel = {
+    name: 'Morning Mimosa',
+    location: 'North Carolina',
+    rating: 5,
+    vacancies: true,
+    rooms: {
+        roomNumber: 7,
+        size: 'Queen Suite',
+        price: 350,
+        booked: false
+    },
+    tags: ['Beautiful', 'Getaway', 'Mimosas']
+}
+// Create My first hotel
+
+// Hotel.create(myFirstHotel, (error, hotel) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log(hotel);
+//     }
+//     db.close()
+// })
+
+
+// Find all the hotels, and logging the entire list
+
+// Hotel.find((err, hotels) => {
+//     console.log(hotels);
+//     db.close()
+// })
+
+
+// Finding just the names of the hotels
+
+// Hotel.find({}, 'name -_id', (err, hotels) => {
+//     console.log(hotels);
+//     db.close()
+// })
+
+
+// Finding just my hotel that I created
+
+// Hotel.find({name: 'Morning Mimosa'}, (err, hotel) => {
+//     console.log(hotel);
+//     db.close()
+// })
+
+
+// Finding all the hotels with vacancies
+
+// Hotel.find({vacancies: true}, (err, hotels) => {
+//     console.log(hotels);
+//     db.close()
+// })
+
+
+// Deleting just 'Hotelicopter'
+
+// Hotel.findOneAndRemove({name: 'Hotelicopter'}, (err, hotel) => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(hotel + ' This is the hotel that pranked you...Sucker');
+//     }
+        // db.close()
+// })
+
+
+// Removing Hilbert's Hotel
+
+// Hotel.findOneAndRemove({name: "Hilbert's Hotel"}, (err, hotel) => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(hotel + ' Sucks to suck');
+//     }
+//         db.close()
+// })
+
+
+// Deleting the hotel in the Rockies
+
+// Hotel.findOneAndRemove({location: 'Colorado Rockies'}, (err, hotel) => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(hotel + ' Had a great location...');
+//     }
+//         db.close()
+// })
+
+
+// Updating the Great Northern's rating
+
+// Hotel.findOneAndUpdate(
+//     {name: 'The Great Northern'},
+//     {rating: 5},
+//     {new: true},
+//     (err, hotel) => {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log(hotel, 'Great Work, you ARE the Great Northern I guess!');
+//         }
+//             db.close()
+//     })
+
+
+// Motel Bambi no longer has vacancies
+
+// Hotel.findOneAndUpdate(
+//     {name: 'Motel Bambi'},
+//     {vacancies: false},
+//     {new: true},
+//     (err, hotel) => {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log(hotel, 'Damn, I really wanted to stay here');
+//         }
+//         db.close()
+//     }
+// )
+
+
+// Changing the rating of White Bay
+
+// Hotel.findOneAndUpdate(
+//     {location: 'White Bay, Oregon'},
+//     {rating: 2},
+//     {new: true},
+//     (err, hotel) => {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log(hotel, 'Wow, just wow. Really slipping');
+//         }
+//         db.close()
+//     }
+// )
