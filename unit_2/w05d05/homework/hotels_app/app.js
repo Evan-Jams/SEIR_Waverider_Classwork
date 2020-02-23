19b3bfe0b4cfab1ef92fe0435ea291b92a3d894a
Evan James <thevan777@gmail.com>
2020-02-22 22:12:08 -0500

    Set up main structure for hotel app

---
+++ unit_2/w05d05/homework/hotels_app/app.js
@@ -0,0 +1,40 @@
+// dependencies
+const mongoose = require('mongoose')
+const db = mongoose.connection
+
+const mongoURI = 'mongodb:localhost:27017/hotel'
+
+const Hotel = require('./models/hotel.js')
+const hotelSeed = require('./models/seed.js')
+
+// Connection to mongo
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}, () => {
+    console.log('Yo, you are connected homie');
+})
+
+// error / success
+db.on('error', (err) =>
+    console.log(err.message + ' mongo is not running')
+)
+db.on('connected', () =>
+    console.log('mongo connected: ', mongoURI)
+)
+db.on('disconnected', () =>
+    console.log('mongo disconnected')
+)
+
+// Hotel.create(hotelSeed, (err, data) => {
+//   if (err) console.log(err.message)
+//   console.log('added provided hotel data')
+// })
+
+// Hotel.collection.drop()
+
+// Hotel.countDocuments({}, (err, data) => {
+//   if (err) console.log(err.message)
+//   console.log(`There are ${data} hotels in this database`)
+// })
---
+++ unit_2/w05d05/homework/hotels_app/models/hotel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const hotelSchema = new Schema (
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        location: true,
+        rating: {
+            type: Number,
+            maxValue: 5
+        },
+        vacancies: {
+            type: Boolean,
+        },
+        tags: [String],
+        rooms: [{
+            roomNumber: Number,
+            size: String,
+            price: Number,
+            booked: Boolean
+        }]
+    },
+    {timestamps: true}
+)
+
+const Hotel = mongoose.model('Hotel', hotelSchema)
+
+module.exports = Hotel
