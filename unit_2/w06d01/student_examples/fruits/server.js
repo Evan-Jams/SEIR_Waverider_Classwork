2dc14b8f87c6f6a2290962b6c5686363e02b0360
Evan James <thevan777@gmail.com>
2020-02-24 16:52:54 -0500

    work from the day

--- unit_2/w06d01/student_examples/fruits/server.js
+++ unit_2/w06d01/student_examples/fruits/server.js
@@ -1,22 +1,77 @@
 const express = require('express')
+const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const app = express()
+const Fruit = require('./models/fruits.js')
+
+
 const PORT = 3000
 /* End Variables */
 
-/* Presentational Routes ******************************/
+/* Express Middleware
+body parser
+static
+method override
+*/
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+
+
+mongoose.connect('mongodb://localhost:27017/basiccrud', { useNewUrlParser: true, unifiedTopolgy: true });
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
+// Presentational Routes ******************************/
 // Index
 app.get('/fruits', (req, res) => {
-    res.send('Fruits')
-})
+    Fruit.find({}, (error, allFruits) => {
+        if (error) {
+            res.send('You done messed up bro')
+        }
+        console.log(allFruits);
+        res.render('index.ejs', {
+            fruits: allFruits
+        });
+    });
+});
 
 // New
 app.get('/fruits/new', (req, res) => {
     res.render('new.ejs')
 })
 
-// Show
-app.get('/', (req, res) => {
+// Seed
+app.get('/fruits/seed', (req, res)=>{
+    Fruit.create([
+        {
+            name:'grapefruit',
+            color:'pink',
+            readyToEat:true
+        },
+        {
+            name:'grape',
+            color:'purple',
+            readyToEat:false
+        },
+        {
+            name:'avocado',
+            color:'green',
+            readyToEat:true
+        }
+    ], (err, data)=>{
+        res.redirect('/fruits');
+    })
+});
 
+// Show
+app.get('/fruits/:id', (req, res) => {
+    Fruit.findById(req.params.id, (err, foundFruit) => {
+        res.render('show.ejs', {
+            fruit: foundFruit
+        })
+    })
 })
 
 // Edit
@@ -28,8 +83,15 @@ app.get('/', (req, res) => {
 
 /* Functional Routes ********************************/
 // Create
-app.post('/', (req, res) => {
-
+app.post('/fruits', (req, res) => {
+    if(req.body.readyToEat === 'on'){
+        req.body.readyToEat = true;
+    } else {
+        req.body.readyToEat = false;
+    }
+    Fruit.create(req.body, (error, createdFruit) => {
+        res.redirect('/fruits')
+    })
 })
 
 // Update
@@ -37,8 +99,9 @@ app.put('/', (req, res) => {
 
 })
 // Delete
-app.delete('/', (req, res) => {
-
+app.delete('/fruits/:id', (req, res) => {
+    // Fruit[req.params.id].splice(Fruit[req.params.id], 1)
+    res.redirect('/fruits')
 })
 
 
