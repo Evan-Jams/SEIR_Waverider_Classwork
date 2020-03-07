ca387b25e4d1d2de3b874ae2afee2ac62ffeeb54
Evan James <thevan777@gmail.com>
2020-03-07 14:21:15 -0500

    about half way through the first part of homework

--- unit_3/w07d05/homework/esnext.js
+++ unit_3/w07d05/homework/esnext.js
@@ -17,25 +17,65 @@ const carObj = {
   }
 }
 
-// Go through the MDN docs, find one method that is standard, one that is obsolete, one that is depreciated, and one that is experimental and write down he name and icon used for each type.
+const {make, model, year} = carObj
+const {honk} = carObj
+
+// console.log(make, model, year);
+// honk()
+
+
+// Go through the MDN docs, find one method that is standard, one that is obsolete, one that is depreciated, and one that is experimental and write down the name and icon used for each type.
+
+//****************************************************************
+
+// Object.eval() is obsolete, uses a trash can icon
+// Object.assign() is standard, no icon
+// Object.__defineSetter__() is deprecated, thumbs down icon
+// AsyncIterator() is experimental, Beaker icon
+
+//****************************************************************
 
 // Get the min of an array of numbers, if it is negative return 'the min is negative', if its positive return 'the min is positive'.
 let arr1 = [2, 1, 10, 6, -2, 1]
 let arr2 = [1, 7, 3, 0, 14, 11]
 
-// Write a function that logs hello world if there is no argument provided. Otherwise it should log the argument provided (string) ex. greetin('Brendan') => hello Brendan.
+// const findMin = (arr) => {
+//     let min = Math.min(...arr);
+//     // console.log(min);
+//     if (min >= 0) {
+//         console.log('The minimum is positive (though it is technically not because the minimum is 0)');
+//     } else {
+//         console.log('The minimum is negative');
+//     }
+// }
+//
+// findMin(arr1);
+// findMin(arr2);
+
+
+// Write a function that logs hello world if there is no argument provided. Otherwise it should log the argument provided (string) ex. greeting('Brendan') => hello Brendan.
+// const greeting = (arg) => {
+//     if (!arg) {
+//         console.log('hello world');
+//     } else {
+//         console.log('hello', arg);
+//     }
+// }
+//
+// greeting('Brendan')
+// greeting()
 
 // Fix the for loop so that Timmy gets a golden key.
 
-let belongings = [
-  'boomerang', 'keyboard', 'tent', 'Pepto Bismol', 'pointy hat'
-]
-let key = 'a golden key'
-for (key of belongings) {
-  // console.log('Timmy checks his satchel and finds', key)
-}
-belongings.push(key)
-// console.log('Timmy now has they gold key', belongings[belongings.length -1] === 'a golden key')
+// let belongings = [
+//   'boomerang', 'keyboard', 'tent', 'Pepto Bismol', 'pointy hat'
+// ]
+// let key = 'a golden key'
+// for (item of belongings) {
+//   console.log('Timmy checks his satchel and finds', item)
+// }
+// belongings.push(key)
+// console.log('Timmy now has the gold key', belongings[belongings.length -1] === 'a golden key')
 
 // Create an empty object and using the new ES6 syntax insert the given variables into the object
 
@@ -43,7 +83,10 @@ const rAndR = 'tent'
 const healing = 'pepto bismol'
 const weapon= 'boomerang'
 const music = 'keyboard'
-const  style = 'pointy hat'
+const style = 'pointy hat'
+const emptyObj = {rAndR, healing, weapon, music, style}
+// console.log(emptyObj);
+
 
 // Use the rest operator to take any number of arguments, get the strings, sort them alphabetically, and return them as one string.
 
@@ -65,6 +108,6 @@ const panagram = ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy'
 
 //=============== HUNGRY FOR MORE ===============//
 
-// Given an array test to see if all values in the array are uniqe or not.
+// Given an array test to see if all values in the array are unique or not.
 
 // Solve https://projecteuler.net/problem=2 using es6 syntax (copy and paste the link into your browser).
