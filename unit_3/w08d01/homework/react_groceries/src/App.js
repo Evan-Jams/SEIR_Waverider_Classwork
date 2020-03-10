50edbf170ff76e9a80d7ab76f61e514c6b0c23db
Evan James <thevan777@gmail.com>
2020-03-09 22:36:51 -0400

    working on getting checkbox to work

--- unit_3/w08d01/homework/react_groceries/src/App.js
+++ unit_3/w08d01/homework/react_groceries/src/App.js
@@ -1,10 +1,89 @@
 import React, { Component } from 'react';
-
+import groceryItems from './data'
 
 class App extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            groceryItems: groceryItems,
+            item: '',
+            brand: '',
+            units: '',
+            quantity: 0,
+            isPurchased: false,
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.toggleCheckbox = this.toggleCheckbox.bind(this)
+    }
+    handleChange(event){
+        this.setState({ [ event.target.id ]: event.target.value })
+    }
+    handleSubmit(event){
+        event.preventDefault();
+        const newItem = {
+            item: this.state.item,
+            brand: this.state.brand,
+            units: this.state.units,
+            quantity: this.state.quantity,
+            isPurchased: this.state.isPurchased
+        }
+        this.setState(
+            {
+                groceryItems: [ newItem, ...this.state.groceryItems ],
+                item: '',
+                brand: '',
+                units: '',
+                quantity: 0,
+                isPurchased: false,
+            },
+            () => {
+                console.log('adding an item', newItem);
+            }
+    )
+        console.log('the new item is', newItem);
+    }
+    toggleCheckbox() {
+        this.setState({isPurchased: !this.state.isPurchased})
+    }
     render() {
         return (
-            <h1>Hi</h1>
+            <div>
+            <h1>Grocery List</h1>
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="item"> Item: </label>
+                    <input type="text" value={this.state.item} onChange={this.handleChange} id="item"/>
+                    <br />
+                <label htmlFor="brand"> Brand: </label>
+                    <input type="text" value={this.state.brand} onChange={this.handleChange} id="brand"/>
+                    <br />
+                <label htmlFor="units"> Units: </label>
+                    <input type="text" value={this.state.units} onChange={this.handleChange} id="units"/>
+                    <br />
+                <label htmlFor="quantity"> Quantity: </label>
+                    <input type="number" value={this.state.quantity} onChange={this.handleChange} id="quantity"/>
+                    <br />
+                <label htmlFor="isPurchased"> Purchased ?: </label>
+                    <input type="checkbox" onChange={this.toggleCheckbox} defaultChecked={this.state.isPurchased} id="isPurchased"/>
+                    <br />
+                <input type="submit" />
+            </form>
+                <ul>
+                    {this.state.groceryItems.map((item, i) =>
+                        <li key={ i }>
+                            <h2>Item: {item.item}</h2>
+                            <h4>Brand: {item.brand}</h4>
+                            <h4>Units: {item.units}</h4>
+                            <h4>Purchased ?: {
+                                    item.isPurchased
+                                    ? 'true'
+                                    : 'false'
+                                }
+                            </h4>
+                        </li>
+                    )}
+                </ul>
+            </div>
         )
     }
 }
---
+++ unit_3/w08d01/homework/react_groceries/src/data.js
@@ -0,0 +1,25 @@
+const groceryItems = [
+    {
+        item: 'Broccoli',
+        brand: 'Earth',
+        units: 'ounces',
+        quantity: 6,
+        isPurchased: false
+    },
+    {
+        item: 'Cool Ranch Doritos',
+        brand: 'Lay\'s',
+        units: 'bags',
+        quantity: 2,
+        isPurchased: false
+    },
+    {
+        item: 'Chicken Pot Pie',
+        brand: 'Big Y',
+        units: 'container',
+        quantity: 100,
+        isPurchased: false
+    },
+]
+
+export default groceryItems;
