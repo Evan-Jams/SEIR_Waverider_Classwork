6e2a169401cd10644555f5caa9f3453eefd197cd
Evan James <thevan777@gmail.com>
2020-03-11 09:10:44 -0400

    refactored code to update independently by moving what was in app to divThree

--- unit_3/w08d02/homework/doctor_who_hw/src/App.js
+++ unit_3/w08d02/homework/doctor_who_hw/src/App.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
 import DivOne from './components/DivOne'
-import DivTwo from './components/DivTwo'
 
 
 class App extends Component {
-    constructor (props){
-        super(props)
-        this.state = {
-            tardis: {
-                name: 'Time and Relative Dimension in Space',
-                caps: false,
-            }
-        }
-        this.changeIt = this.changeIt.bind(this)
-    }
-    changeIt (text) {
-        if (this.state.tardis.caps) {
-            this.setState({
-                tardis: {
-                    name: text.toLowerCase(),
-                    caps: false
-                }
-            })
-        } else {
-            this.setState({
-                tardis: {
-                    name: text.toUpperCase(),
-                    caps: true
-                }
-            })
-        }
-    }
+
     render() {
         return (
             <div>
-                <DivOne tardis={this.state.tardis} changeIt={this.changeIt}/>
+                <DivOne tardis={this.props.tardis} changeIt={this.changeIt}/>
             </div>
         )
     }
--- unit_3/w08d02/homework/doctor_who_hw/src/components/DivThree.js
+++ unit_3/w08d02/homework/doctor_who_hw/src/components/DivThree.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react'
 
 class DivThree extends Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            tardis: {
+                name: 'Time and Relative Dimension in Space',
+                caps: false,
+            }
+        }
+        this.changeIt = this.changeIt.bind(this)
+    }
+    changeIt (text) {
+        if (this.state.tardis.caps) {
+            this.setState({
+                tardis: {
+                    name: text.toLowerCase(),
+                    caps: false
+                }
+            })
+        } else {
+            this.setState({
+                tardis: {
+                    name: text.toUpperCase(),
+                    caps: true
+                }
+            })
+        }
+    }
     render() {
         return(
             <div>
-                <h3 onClick={() => {this.props.changeIt(this.props.tardis.name)}} >{this.props.tardis.name}
+                <h3 onClick={() => {this.changeIt(this.state.tardis.name)}} >{this.state.tardis.name}
                 </h3>
             </div>
         )
