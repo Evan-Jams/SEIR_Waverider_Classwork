500c877a1c16910f9b09ce17fbcc2640abbb952b
Evan James <thevan777@gmail.com>
2020-03-13 17:17:38 -0400

    classwork from the day

--- unit_3/w08d04/student_examples/holidays/holidays-front-end/src/App.js
+++ unit_3/w08d04/student_examples/holidays/holidays-front-end/src/App.js
@@ -3,9 +3,9 @@ import './css/normalize.css'
 import './css/skeleton.css'
 import './css/index.css'
 import NewForm from './components/NewForm.js'
+import Show from './components/Show.js'
 // import ballons from './images/two-balloon-icons-68911.png'
 // import pencil from './images/simpleiconDOTcom-pen-15-64x64.png'
-// import Show from './components/Show.js'
 // import UpdateForm from './components/UpdateForm.js'
 let baseURL = ''
 
@@ -26,12 +26,14 @@ class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            holidays: []
+            holidays: [],
+            holiday: null
         }
         this.getHolidays = this.getHolidays.bind(this)
         this.handleAddHoliday = this.handleAddHoliday.bind(this)
         this.deleteHoliday = this.deleteHoliday.bind(this)
         this.toggleCelebrated = this.toggleCelebrated.bind(this)
+        this.getHoliday = this.getHoliday.bind(this)
     }
     componentDidMount(){
         this.getHolidays()
@@ -89,17 +91,22 @@ class App extends React.Component {
             console.error(e);
         }
     }
+
+    getHoliday(holiday) {
+        this.setState({holiday: holiday})
+    }
  render () {
    return (
      <div className='container'>
       <h1>Holidays! Celebrate!</h1>
       <NewForm handleAddHoliday={this.handleAddHoliday} baseURL={baseURL}/>
       <table>
+
         <tbody>
           { this.state.holidays.map(holiday => {
               return (
-                <tr key={holiday._id}>
-                  <td id={holiday._id} onClick={()=> {this.toggleCelebrated(holiday)}}> {holiday.name } and it's {holiday.celebrated ? 'celebrated' : 'not celebrated'}</td>
+                <tr key={holiday._id} onMouseOver={() => this.getHoliday(holiday)}>
+                  <td id={holiday._id} onDoubleClick={()=> {this.toggleCelebrated(holiday)}}> {holiday.name} and it's {holiday.celebrated ? 'celebrated' : 'not celebrated'}</td>
                   <td></td>
                   <td onClick={ () => { this.deleteHoliday(holiday._id) } }>X</td>
                 </tr>
@@ -108,6 +115,9 @@ class App extends React.Component {
           }
         </tbody>
       </table>
+      { this.state.holiday
+          ? <Show holiday={this.state.holiday} />
+          : null }
      </div>
    )
  }
