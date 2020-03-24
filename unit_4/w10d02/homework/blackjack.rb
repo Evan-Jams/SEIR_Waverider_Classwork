67296ed5352965dca6b473a27eca54ea5a0a4e69
Evan James <thevan777@gmail.com>
2020-03-24 18:07:45 -0400

    added card and deck classes, also got the deck to deal 2 random cards to a player (as a test)

--- unit_4/w10d02/homework/blackjack.rb
+++ unit_4/w10d02/homework/blackjack.rb
@@ -5,45 +5,77 @@
 
 class Player
 
-  attr_reader :name, :bank, :hand
+  attr_accessor :name, :bank, :hand, :count
 
-  def initialize name, bank = 100
+  def initialize name, bank = 100, count = 0
     @name = name
     @bank = bank
     @hand = []
+    @count = count
   end
 
 end
 
 player1 = Player.new "Evan"
+dealer = Player.new "The House", 10000
+# p player1
+p dealer
 
-p player1
+###################################################################
+# card and deck classes
+###################################################################
 
-class The_House
+class Card
 
-  attr_reader :name, :bank, :hand
+  attr_accessor :face, :suit, :value
 
-  def initialize name, bank = 10000
-    @name = name
-    @bank = bank
-    @hand = []
+  def initialize face, suit, value
+    @face = face
+    @suit = suit
+    @value = value
+  end
+  def generate_card (player)
+    new_card = Card.new face, suit, value
+    player.hand << new_card
+    player.count = player.count + new_card.value
   end
 
 end
 
-dealer = The_House.new "Dirty Dan"
 
-p dealer
+class Deck
 
-###################################################################
-# Game set-up
-###################################################################
+  attr_accessor :faces, :suits, :cards
 
-# Cards deck
-cards = []
+  def initialize
+    @faces = [*(2..10), "Jack", "Queen", "King", "Ace"]
+    @suits = ["clubs", "spades", "hearts", "diamonds"]
+    @cards = []
 
+    @faces.each do |face|
+      if face.class == Integer
+        value = face
+      elsif face == "Ace"
+        value = 11
+      else
+        value = 10
+      end
+      @suits.each do |suit|
+        @cards << Card.new(face, suit, value)
+      end
+    end
+    @cards.shuffle!
+  end
 
+  def deal player, num_of_cards
+    num_of_cards.times { @cards.shift.generate_card(player) }
+  end
+end
 
+master_deck = Deck.new
+
+master_deck.deal(player1, 2)
+p player1.hand
 
 
 
