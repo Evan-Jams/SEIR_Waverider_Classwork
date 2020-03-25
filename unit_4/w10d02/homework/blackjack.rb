14ef73fae75d7f54a4aeb94d999fd3eed4343bbe
Evan James <thevan777@gmail.com>
2020-03-24 20:17:28 -0400

    working on going through the conditionals and adding to the players count

--- unit_4/w10d02/homework/blackjack.rb
+++ unit_4/w10d02/homework/blackjack.rb
@@ -19,7 +19,7 @@ end
 player1 = Player.new "Evan"
 dealer = Player.new "The House", 10000
 # p player1
-p dealer
+# p dealer
 
 ###################################################################
 # card and deck classes
@@ -67,17 +67,63 @@ class Deck
     @cards.shuffle!
   end
 
-  def deal player, num_of_cards
+  def deal (player, num_of_cards)
     num_of_cards.times { @cards.shift.generate_card(player) }
   end
 end
 
 master_deck = Deck.new
 
-master_deck.deal(player1, 2)
-p player1.hand
+
+# p player1.hand
+
+###################################################################
+# Game logic
+###################################################################
 
 
+puts "What is your name player? "
+player1.name = gets.chomp
+puts "Welcome to the Blackjack table #{player1.name}! You have $#{player1.bank} dollars in your pocket"
+
+# def hit_after_deal
+#   master_deck.deal(player1, 1)
+#   puts "You drew a ( #{player1.hand[2].face} of #{player1.hand[2].suit} )"
+# end
+
+
+loop do
+  puts "Deal (1), or Quit (2)"
+  input = gets.chomp
+  if input == '1'
+    master_deck.deal(player1, 2)
+    master_deck.deal(dealer, 2)
+
+    puts "You currently are holding a ( #{player1.hand[0].face} of #{player1.hand[0].suit} ) and a ( #{player1.hand[1].face} of #{player1.hand[1].suit} ). Your count is #{player1.hand[0].value + player1.hand[1].value} "
+
+    puts "The dealer is showing a ( #{dealer.hand[0].face} of #{dealer.hand[0].suit} )"
+
+    loop do
+      puts "Draw another (1) or Stay (2)"
+      input2 = gets.chomp
+      if input2 == '1'
+        master_deck.deal(player1, 1)
+        puts "You drew a ( #{player1.hand[2].face} of #{player1.hand[2].suit} )"
+        break
+      elsif input2 == '2'
+        puts "You chose to stay"
+        puts "The dealer reveals a ( #{dealer.hand[1].face} of #{dealer.hand[1].suit} ). His count is #{dealer.hand[0].value + dealer.hand[1].value}"
+      end
+    end
+
+
+  elsif input == '2'
+    puts "Sad to see you leave, we really wanted your money"
+    break
+  else
+    puts "Sorry, we didn't understand your response, try that again..."
+  end
+end
 
 
 
