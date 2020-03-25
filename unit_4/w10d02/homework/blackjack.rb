1144e0304fb0f374936c3a99db97f4099e1a52a5
Evan James <thevan777@gmail.com>
2020-03-24 22:48:06 -0400

    Finished game logic and refactored slightly. No known bugs

--- unit_4/w10d02/homework/blackjack.rb
+++ unit_4/w10d02/homework/blackjack.rb
@@ -7,19 +7,29 @@ class Player
 
   attr_accessor :name, :bank, :hand, :count
 
-  def initialize name, bank = 100, count = 0
+  def initialize name, bank, count = 0
     @name = name
     @bank = bank
     @hand = []
     @count = count
   end
 
+  def defeats opponent
+    puts "#{self.name} wins this round!"
+    self.bank += 10
+    puts "#{self.name.capitalize} is now at $#{self.bank}"
+    opponent.bank -= 10
+    puts "#{opponent.name.capitalize} is now at $#{opponent.bank}"
+    self.hand.clear
+    self.count = 0
+    opponent.hand.clear
+    opponent.count = 0
+  end
+
 end
 
-player1 = Player.new "Evan"
+player1 = Player.new "Evan", 100
 dealer = Player.new "The House", 10000
-# p player1
-# p dealer
 
 ###################################################################
 # card and deck classes
@@ -74,23 +84,13 @@ end
 
 master_deck = Deck.new
 
-
-# p player1.hand
-
 ###################################################################
 # Game logic
 ###################################################################
 
-
 puts "What is your name player? "
 player1.name = gets.chomp
 puts "Welcome to the Blackjack table #{player1.name}! You have $#{player1.bank} dollars in your pocket"
-
-# def hit_after_deal
-#   master_deck.deal(player1, 1)
-#   puts "You drew a ( #{player1.hand[2].face} of #{player1.hand[2].suit} )"
-# end
-
 
 loop do
   puts "Deal (1), or Quit (2)"
@@ -99,20 +99,66 @@ loop do
     master_deck.deal(player1, 2)
     master_deck.deal(dealer, 2)
 
-    puts "You currently are holding a ( #{player1.hand[0].face} of #{player1.hand[0].suit} ) and a ( #{player1.hand[1].face} of #{player1.hand[1].suit} ). Your count is #{player1.hand[0].value + player1.hand[1].value} "
+    puts "You currently are holding a ( #{player1.hand[0].face} of #{player1.hand[0].suit} ) and a ( #{player1.hand[1].face} of #{player1.hand[1].suit} ). Your count is #{player1.count} "
 
-    puts "The dealer is showing a ( #{dealer.hand[0].face} of #{dealer.hand[0].suit} )"
+    puts "The dealer is showing a ( #{dealer.hand[0].face} of #{dealer.hand[0].suit}, the dealer's count is #{dealer.count})"
 
     loop do
       puts "Draw another (1) or Stay (2)"
       input2 = gets.chomp
+
       if input2 == '1'
         master_deck.deal(player1, 1)
-        puts "You drew a ( #{player1.hand[2].face} of #{player1.hand[2].suit} )"
-        break
+        puts "You drew a ( #{player1.hand.last.face} of #{player1.hand.last.suit} )"
+        puts "Your count is now #{player1.count}"
+
+        if player1.count == 21
+          puts "YOU'VE GOT BLACKJACK!"
+          break
+        elsif player1.count > 21
+          puts "You busted! Sorry, we'll gladly take your $10 dollars"
+          dealer.defeats player1
+          break
+        end
+
       elsif input2 == '2'
         puts "You chose to stay"
-        puts "The dealer reveals a ( #{dealer.hand[1].face} of #{dealer.hand[1].suit} ). His count is #{dealer.hand[0].value + dealer.hand[1].value}"
+        puts "The dealer reveals a ( #{dealer.hand[1].face} of #{dealer.hand[1].suit} ). His count is #{dealer.count}"
+
+        loop do
+          if dealer.count == 21
+            puts "Dealer Got Blackjack!"
+            dealer.defeats player1
+            break
+
+          elsif dealer.count < 17
+            master_deck.deal(dealer, 1)
+            puts "The dealer reveals a ( #{dealer.hand.last.face} of #{dealer.hand.last.suit} ). His count is #{dealer.count}"
+
+          elsif dealer.count >= 17 && dealer.count < 21
+            puts "The dealer's count is #{dealer.count}"
+
+            if dealer.count > player1.count
+              puts "You have lost this hand and $10 dollars! Sorry, not sorry"
+              dealer.defeats player1
+              break
+
+            elsif dealer.count < player1.count
+              player1.defeats dealer
+              break
+
+            elsif dealer.count > 21
+              puts "Dealer has busted! You win this round"
+              player1.defeats dealer
+              break
+
+            else
+              puts "You tied, no money lost or gained"
+              break
+            end
+          end
+        end
+        break
       end
     end
 
@@ -123,6 +169,7 @@ loop do
   else
     puts "Sorry, we didn't understand your response, try that again..."
   end
+  break
 end
 
 
