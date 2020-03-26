1f05f5b13eec1a8a8d489b9fad0d72221ac4ff2b
Evan James <thevan777@gmail.com>
2020-03-26 15:43:40 -0400

    set up user schema and id to todos

--- unit_4/w10d04/student_examples/todo_app_api/db/schema.rb
+++ unit_4/w10d04/student_examples/todo_app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_26_154342) do
+ActiveRecord::Schema.define(version: 2020_03_26_192744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2020_03_26_154342) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.boolean "completed"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
   end
 
 end
