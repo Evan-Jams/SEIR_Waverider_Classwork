615e08c17438978676a5dbef179de1fb8f11d857
Evan James <thevan777@gmail.com>
2020-03-26 22:49:21 -0400

    Commit 11: relations

---
+++ unit_4/w10d04/homework/matrix_app_api/db/migrate/20200327024140_change_characters_table.rb
@@ -0,0 +1,5 @@
+class ChangeCharactersTable < ActiveRecord::Migration[6.0]
+  def change
+    add_column :characters, :vehicle_id, :integer
+  end
+end
--- unit_4/w10d04/homework/matrix_app_api/db/seeds.rb
+++ unit_4/w10d04/homework/matrix_app_api/db/seeds.rb
@@ -6,11 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Character.create([
+# Character.create([
+#
+# 	{ name: "Neo", description: "Thomas A. Anderson was born in Lower Downtown, Capital City, USA on March 11, 1962" },
+# 	{ name: "Trinity", description: "Trinity is a computer programmer and a hacker who has escaped from the Matrix" },
+# 	{ name: "Morpheus", description: "The name Morpheus is that of the god of dreams in Greek mythology, which is consistent with the character's involvement with the 'dreaming' of the Matrix." },
+# 	{ name: "Agent Smith", description: "Smith began as an Agent, an AI program in the Matrix programmed to keep order within the system" },
 
-	{ name: "Neo", description: "Thomas A. Anderson was born in Lower Downtown, Capital City, USA on March 11, 1962" },
-	{ name: "Trinity", description: "Trinity is a computer programmer and a hacker who has escaped from the Matrix" },
-	{ name: "Morpheus", description: "The name Morpheus is that of the god of dreams in Greek mythology, which is consistent with the character's involvement with the 'dreaming' of the Matrix." },
-	{ name: "Agent Smith", description: "Smith began as an Agent, an AI program in the Matrix programmed to keep order within the system" },
+# ])
 
-])
+Vehicle.create([
+  { name: "Nebuchadnezzar", style: "Spaceship"}
+  ])
