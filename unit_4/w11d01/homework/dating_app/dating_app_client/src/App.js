98b7c63d3f58389884c23599799e7a07cb7a2dd5
Evan James <thevan777@gmail.com>
2020-03-30 18:22:20 -0400

    set up Cors

--- unit_4/w11d01/homework/dating_app/dating_app_client/src/App.js
+++ unit_4/w11d01/homework/dating_app/dating_app_client/src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Daters from './components/Hello.js'
 
-function App() {
-  return (
-    <div className="App">
-      <Daters />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      users: []
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <Daters />
+      </div>
+    )
+  }
 }
 
 export default App;
--- unit_4/w11d01/homework/dating_app/dating_app_client/src/components/Hello.js
+++ unit_4/w11d01/homework/dating_app/dating_app_client/src/components/Hello.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react'
 
 class Daters extends Component {
-  
+  constructor(props){
+    super(props)
+    this.state = {
+      daters: []
+    }
+  }
+  componentDidMount(){
+    this.getDaters()
+  }
   getDaters(){
-    fetch('localhost:3000')
-    .then( daters => console.log(daters))
+    fetch('http://localhost:3000/users')
+    .then(response => response.json())
+    .then(jsonDaters => this.setState({daters: jsonDaters}))
+    .catch(error => console.error(error))
   }
   render(){
     return(
