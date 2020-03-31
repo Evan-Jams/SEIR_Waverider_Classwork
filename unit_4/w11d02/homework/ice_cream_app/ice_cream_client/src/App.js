50572a4906de535343fe4131c2eb3c9b8c189029
Evan James <thevan777@gmail.com>
2020-03-31 18:28:05 -0400

    MVP finished for pints graph

--- unit_4/w11d02/homework/ice_cream_app/ice_cream_client/src/App.js
+++ unit_4/w11d02/homework/ice_cream_app/ice_cream_client/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import LineChart from './components/LineChart'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>This is where Ice Creams Flourish</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      iceCreams: []
+    }
+    this.getIceCreams = this.getIceCreams.bind(this)
+  }
+  componentDidMount(){
+    this.getIceCreams()
+  }
+  async getIceCreams(){
+    try{
+      const response = await fetch('/ice_creams')
+      const iceCreams = await response.json()
+      this.setState({ iceCreams:[...iceCreams] })
+      console.log(this.state.iceCreams)
+    }catch (err){
+      console.error(err)
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>This is where Ice Creams Flourish</h1>
+        <LineChart id={1}/>
+        <LineChart id={2}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ unit_4/w11d02/homework/ice_cream_app/ice_cream_client/src/components/LineChart.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react'
+import Chart from 'chart.js'
+
+
+class LineChart extends Component {
+  componentDidMount(){
+	   this.getData(this.props.id)
+  }
+  async getData(id){
+    try{
+      const response = await fetch(`/ice_creams/${id}`)
+      const iceCream = await response.json()
+      const preparedData = this.prepareData(iceCream)
+      this.createChart(preparedData)
+    } catch(err){
+      console.error(err)
+    }
+  }
+  prepareData (data) {
+    const chartData = {
+        labels: [],
+        datasets: [
+          {
+            label: 'Pints Sold',
+            data: []
+          },
+      		{
+      			label: 'Pints Made',
+      			data: []
+      		}
+        ]
+    }
+    data.pints.forEach(pint => {
+     chartData.labels.push(pint.month)
+     chartData.datasets[0].data.push(pint.pints_sold)
+     chartData.datasets[1].data.push(pint.pints_made)
+    })
+    return chartData
+   }
+
+  createChart(data){
+    const ctx = document.querySelector(`#pints-${this.props.id}`)
+    const pintsChart = new Chart(ctx, {
+      type: 'line',
+      data: data
+    })
+    return pintsChart
+  }
+  render(){
+    return(
+      <>
+        <h1>{}</h1>
+        <canvas id={`pints-${this.props.id}`} width="300" height="100"></canvas>
+      </>
+    )
+  }
+}
+
+export default LineChart;
